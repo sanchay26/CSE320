787b6e46d731ff12bf684e6af153d0448444e5e3
sanchay <saagrawal>
2016-08-31 22:19:32 -0400

    Edited test.c file

--- hw0/test.c
+++ hw0/test.c
@@ -1,8 +1,15 @@
-// hello.c
+/* Hello Students! program */
 #include <stdlib.h>
 #include <stdio.h>
-
-int main(int argc, char *argv[]) {
-	printf("Code pulled from remote repository.\n");
-	return EXIT_SUCCESS;
+int
+main(
+int
+argc,
+char
+* argv[]) {
+fprintf(stdout, "%s
+\n
+", "Hello Students!");
+return
+EXIT_SUCCESS;
 }
