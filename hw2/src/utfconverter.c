e84caafe167112537f3331ff35a00f3291c457a9
sanchay <saagrawal>
2016-09-18 23:02:21 -0400

    Working On Sparky

--- hw2/src/utfconverter.c
+++ hw2/src/utfconverter.c
@@ -1,29 +1,29 @@
 #include "../include/utfconverter.h"
 #include <sys/stat.h>
-//#include "struct.txt" 
+
 char* filename;
 endianness source;
 endianness conversion;
 
-//int test=0;
 int main(int argc, char** argv)
-//char** argv;
 {
 	/* After calling parse_args(), filename and conversion should be set. */
+	int fd;
+	unsigned char buf[4];
+	int rv;
+	Glyph* glyph = malloc(sizeof(Glyph));
 
 	parse_args(argc, argv);
-	int fd ;
+	
 	fd = open(filename, O_RDONLY); 
-	unsigned char buf[4]; 
+	 
 	buf[0]=0;
 	buf[1]=0;
 	buf[2]=0;
 	buf[3]=0;
-	int rv ;
 	rv= 0;
-	Glyph* glyph = malloc(sizeof(Glyph)); 
 
-	//test = open("rsrc/test.txt", O_CREAT | O_WRONLY);
+	/*test = open("rsrc/test.txt", O_CREAT | O_WRONLY);*/
 	/* Handle BOM bytes for UTF16 specially. 
          * Read our values into the first and second elements. */
 	if((rv = read(fd, &buf[0], 1)) == 1 && (rv = read(fd, &buf[1], 1)) == 1){ 
@@ -61,16 +61,8 @@ int main(int argc, char** argv)
 			quit_converter(NO_FD); 
 		}
 
-		void* memset_return = memset(glyph, 0, sizeof(Glyph)+1);
-		/* Memory write failed, recover from it: */
-		if(memset_return == NULL){
-			/* tweak write permission on heap memory. */
-			/* Now make the request again. */
-			memset(glyph, 0, sizeof(Glyph)+1);
-		}
 	}
 	
-	//write();
 	/* Now deal with the rest of the bytes.*/
 	while((rv = read(fd, &buf[0], 1)) == 1 &&  (rv = read(fd, &buf[1], 1)) == 1){
 
@@ -82,14 +74,8 @@ int main(int argc, char** argv)
 			write_glyph(swap_endianness(fill_glyph(glyph, buf, source, &fd)));
 		}
 		
-		void* memset_return = memset(glyph, 0, sizeof(Glyph)+1);
-	        /* Memory write failed, recover from it: */
-	        if(memset_return == NULL){
-		        /* tweak write permission on heap memory. */
-		        /* Now make the request again. */
-		        memset(glyph, 0, sizeof(Glyph)+1);
-	        }
 	}
+
 	quit_converter(NO_FD);
 	return 0;
 }
@@ -101,14 +87,10 @@ Glyph* swap_endianness(Glyph* glyph) {
 	glyph->bytes[0] = glyph->bytes[1];
 	glyph->bytes[1]= temporary;
 
-	//glyph->bytes[0] ^= glyph->bytes[1];
-	//glyph->bytes[1] ^= glyph->bytes[0];
 	if(glyph->surrogate){  /* If a surrogate pair, swap the next two bytes. */
 	 	temporary = glyph->bytes[2];
 		glyph->bytes[2] = glyph->bytes[3];
 		glyph->bytes[3]= temporary;
-		//glyph->bytes[2] ^= glyph->bytes[3];
-		//glyph->bytes[3] ^= glyph->bytes[2];
 	}
 	glyph->end = conversion;
 	return glyph;
@@ -116,11 +98,10 @@ Glyph* swap_endianness(Glyph* glyph) {
 
 Glyph* fill_glyph (Glyph* glyph,unsigned char data[4],endianness end,int* fd)  
 {
-	glyph->bytes[0] = data[0];
-	glyph->bytes[1] = data[1];
 	
 	unsigned int bits = 0; 
-
+	glyph->bytes[0] = data[0];
+	glyph->bytes[1] = data[1];
 
 	if(end == LITTLE){
 		bits |= (data[FIRST] + (data[SECOND] << 8));	
@@ -181,7 +162,6 @@ int file_exist (char *filename)
 void write_glyph(Glyph* glyph)
 {
 	if(glyph->surrogate){
-		//printf("%s\n", "Somewhere");
 		write(STDOUT_FILENO, glyph->bytes, SURROGATE_SIZE);
 	} else {
 		write(STDOUT_FILENO, glyph->bytes, NON_SURROGATE_SIZE);
@@ -193,7 +173,6 @@ void parse_args(int argc,char** argv)
 	int option_index, c;
 	char* endian_convert = NULL;
 
-	// Copied from struct.txt
 	static struct option long_options[] = {
 		{"help", no_argument, 0, 'h'},
 		{"h", no_argument, 0, 'h'},
@@ -210,17 +189,16 @@ void parse_args(int argc,char** argv)
 				print_help();
 				break;
 			case 'u':
-				//printf("optarg%s\n",argv[optind]);
 				endian_convert = optarg;
 				printf("***endian***%s\n",endian_convert );
 				if(strcmp(endian_convert,"16BE")==0)
 				{
 					conversion = BIG;
-					//Big Endian Condition
+				
 				}
 				else if(strcmp(endian_convert,"16LE")==0){
 					conversion = LITTLE;
-					//Little Endian Condition
+					
 				}
 				else {
 					printf("%s\n","wrong arguments" );
@@ -261,7 +239,8 @@ void parse_args(int argc,char** argv)
 }
 
 void print_help(void) {
-	for(int i = 0; i < 4; i++){
+	int i;
+	for(i = 0; i < 4; i++){
 		printf("%s",USAGE[i]); 
 	}
 	quit_converter(NO_FD);
